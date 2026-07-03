6c105303590917075fdfbadb4b4e716007096bf2
Tom Clark <the.thomas.j.clark@gmail.com>
2026-07-03 15:03:55 -0400

    Oops I need mutex

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,3 @@
-#![feature(sync_unsafe_cell)]
-#![feature(ptr_as_ref_unchecked)]
-
 mod input_state;
 mod physics;
 mod player_ins_skeleton_ext;
@@ -8,8 +5,7 @@ mod qwop_mod;
 mod rvas;
 
 use std::{
-    cell::SyncUnsafeCell, fs::OpenOptions, os::windows::io::AsRawHandle, ptr::NonNull,
-    time::Duration,
+    fs::OpenOptions, os::windows::io::AsRawHandle, ptr::NonNull, sync::Mutex, time::Duration,
 };
 use windows::Win32::{
     Foundation::{HANDLE, HINSTANCE},
@@ -28,8 +24,6 @@ use fromsoftware_shared::{Program, SharedTaskImpExt};
 use pelite::pe64::{Pe, Rva};
 
 use crate::qwop_mod::QwopMod;
-
-static QWOP_MOD: SyncUnsafeCell<Option<QwopMod>> = SyncUnsafeCell::new(None);
 
 retour::static_detour! {
     static ChrCtrl_UpdatePos: extern "C" fn(NonNull<ChrCtrl>);
@@ -52,14 +46,12 @@ pub extern "C" fn DllMain(module: HINSTANCE, reason: u32) -> bool {
         std::mem::forget(stdout);
     };
 
+    let qwop_mod = Box::leak(Box::new(Mutex::new(QwopMod::default())));
+
     std::thread::spawn(move || {
         let cs_task = CSTaskImp::wait_for_instance(Duration::MAX).unwrap();
         cs_task.run_recurring(
-            |_: &FD4TaskData| {
-                unsafe { QWOP_MOD.get().as_mut_unchecked() }
-                    .get_or_insert_default()
-                    .chr_ins_pre_behavior()
-            },
+            |_: &FD4TaskData| qwop_mod.lock().unwrap().chr_ins_pre_behavior(),
             CSTaskGroupIndex::ChrIns_PreBehavior,
         );
 
@@ -70,12 +62,7 @@ pub extern "C" fn DllMain(module: HINSTANCE, reason: u32) -> bool {
                 .initialize(
                     retour::Function::from_ptr(rva_to_ptr(rvas::CHR_CTRL_UPDATE_POS)),
                     |chr_ctrl| {
-                        QWOP_MOD
-                            .get()
-                            .as_mut_unchecked()
-                            .get_or_insert_default()
-                            .chr_ctrl_update_pos_hook(chr_ctrl);
-
+                        qwop_mod.lock().unwrap().chr_ctrl_update_pos_hook(chr_ctrl);
                         ChrCtrl_UpdatePos.call(chr_ctrl);
                     },
                 )
@@ -87,10 +74,9 @@ pub extern "C" fn DllMain(module: HINSTANCE, reason: u32) -> bool {
                 .initialize(
                     retour::Function::from_ptr(rva_to_ptr(rvas::CHR_INS_BEHAVIOR_SAFE)),
                     |world_chr_man| {
-                        QWOP_MOD
-                            .get()
-                            .as_mut_unchecked()
-                            .get_or_insert_default()
+                        qwop_mod
+                            .lock()
+                            .unwrap()
                             .chr_ins_behavior_safe_hook(world_chr_man);
 
                         ChrIns_BehaviorSafe.call(world_chr_man);
